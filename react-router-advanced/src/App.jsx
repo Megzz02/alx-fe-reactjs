77abd0ab9ccc9ee835e9ae6a878e7e606728865e
Ahmed <ahmedsaleh.ams02@gmail.com>
2024-11-24 23:43:48 +0200

    3

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
